d25d4f912095c94ae5b1af2d8d48c59ea66397de
Daniela Perry <danielaperry2015@gmail.com>
2025-02-06 12:51:55 -0800

    plugin setup file added

---
+++ ch/q2/plugin_setup.py
@@ -0,0 +1,90 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2019--, gemelli development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+
+import qiime2.plugin
+import qiime2.sdk
+import importlib
+
+from ch.q2._visualizer import hulls_plots
+
+from ch import __version__
+
+from gemelli.q2._visualizer import qc_rarefy
+
+from ._type import Hulls
+from ._format import HullsFormat, HullsDirectoryFormat
+
+from qiime2.plugin import (Int, Str, Bool,
+                           Metadata, Visualization)
+from q2_types.ordination import PCoAResults
+from qiime2.plugin import Metadata
+
+
+citations = qiime2.plugin.Citations.load(
+    'citations.bib', package='gemelli')
+
+plugin = qiime2.plugin.Plugin(
+    name='gemelli',
+    version=__version__,
+    website="https://github.com/biocore/gemelli",
+    citations=[citations['Martino2019'],
+               citations['Martino2020']],
+    short_description=('Plugin for Compositional Tensor Factorization'),
+    description=('This is a QIIME 2 plugin supporting Robust Aitchison on '
+                 'feature tables'),
+    package='gemelli')
+
+plugin = qiime2.plugin.Plugin(
+    name='convexhull2',
+    version=__version__,
+    website="https://github.com/dpear/q2-convexhull2",
+    citations=[],
+    short_description=('Plugin for calculating '
+        'and visualizing Convex Hull Volume'),
+    description=('This is a QIIME 2 plugin for '
+        'calculating and visualizing Convex Hull Volume'),
+    package='convexhull2')
+
+plugin.visualizers.register_function(
+    function=hulls_plots,
+    inputs={
+        'ordination': PCoAResults,
+    },
+    parameter_descriptions={
+        'metadata': 'Metadata',
+        'groupc': 'Str',
+        'subjc': 'Str',
+        'timec': 'Str',
+        'axis': 'Bool default True',
+        'rotation': 'Int = default 60',
+        'n_iters': 'Int = default 20',
+        'n_subsamples': 'int = None',
+    },
+
+    input_descriptions={
+        'ordination': 'PCoAResults',
+    },
+    parameters={
+        'metadata': Metadata,
+        'groupc': Str,
+        'subjc': Str,
+        'timec': Str,
+        'axis': Bool = True,
+        'rotation': Int = 60,
+        'n_iters': Int = 20,
+        'n_subsamples': int = None,
+    },
+    name='Calculate all 3 types of plots',
+    description=('Generate a qiime2 visualization '
+                 'that allows for generating and easy download '
+                 'of 3d hulls plot, line plot of group dispersion '
+                 'and boxplot of individual dispersion by group'),
+    citations=[],
+)
+
+importlib.import_module('ch.q2._transformer')
